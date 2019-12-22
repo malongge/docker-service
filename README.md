127fe0dc09eb8d6e5cb7d72b0c4da5c35ea0dedc
malongge <ma_longge@sina.com>
2019-12-22 16:35:25 +0800

    docker service

---
+++ README.md
@@ -0,0 +1,7 @@
+docker network create app_net
+
+windows:
+    eg rabbitmq management: http://192.168.99.100:15672
+
+mac:
+    eg rabbitmq management: http://0.0.0.0:15672
---
+++ local-mysql Session.sql
@@ -0,0 +1,12 @@
+-- use testdb;
+-- CREATE TABLE Persons
+-- (
+-- Id_P int,
+-- LastName varchar(255),
+-- FirstName varchar(255),
+-- Address varchar(255),
+-- City varchar(255)
+-- )
+-- show tables;
+
+INSERT into Persons values (1, '王', '陽明', '吉安', '江西');
